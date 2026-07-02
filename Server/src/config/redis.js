4e65371a9f758427b8684c8a454baa6eb9a35d5b
Udita Singh <udita1294@gmail.com>
2026-07-02 23:08:22 +0530

    type conversion of port done

--- Server/src/config/redis.js
+++ Server/src/config/redis.js
@@ -6,7 +6,7 @@ dotenv.config();
 const client = createClient({
     socket : {
         host : process.env.REDIS_HOST,
-        port : process.env.REDIS_PORT
+        port : Number(process.env.REDIS_PORT)
     },
 });
 

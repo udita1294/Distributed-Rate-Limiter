581f5c632a0b66110b5fc10952c97a7a093a76ef
Udita Singh <udita1294@gmail.com>
2026-07-02 18:59:40 +0530

    redis connection function created

---
+++ Server/src/config/redis.js
@@ -0,0 +1,24 @@
+import {createClient} from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = createClient({
+    socket : {
+        host : process.env.REDIS_HOST,
+        port : process.env.REDIS_PORT
+    },
+});
+
+client.on('connect',()=>{
+    console.log('Redis client connected');
+});
+client.on('error',(err)=>{
+    console.log('Redis client error',err.message);
+});
+
+export async function connectRedis(){
+    await client.connect();
+}
+
+export default client;

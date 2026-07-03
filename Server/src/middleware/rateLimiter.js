2f4a1a900639f87b8c707e138b6d6fd074fdaf7e
Udita Singh <udita1294@gmail.com>
2026-07-03 23:38:46 +0530

    updated rateLimiter middleware

--- Server/src/middleware/rateLimiter.js
+++ Server/src/middleware/rateLimiter.js
@@ -1,28 +1,50 @@
-import {fixedWindow} from '../algorithms/fixedWindow.js'
+
+import defaultConfig from '../config/defaultConfig.js'
+import {getAlgorithm} from '../strategy/algorithmFactory.js'
+import {generateKey} from '../utils/keyGenerator.js'
+import {validateConfig} from '../utils/validateConfig.js'
+
 
 export default function rateLimiter(options = {}){
 
+    // Merge user config with defaults
+    const config = {
+        ...defaultConfig,
+        ...options
+    }
+
+    // Validate once during startup
+    validateConfig(config);
+
     return async(req,res,next)=>{
+        try{
+            // Generate key
+            const key = generateKey(req,config);
+            // get algorithm function based on the config
+            const algorithm = getAlgorithm(config.algorithm);
+            // execute the algorithm with the key and config
+            const result = await algorithm(key,config);
 
-        const key = req.ip;
+            // Standard Rate Limit Headers
+            res.setHeader('X-RateLimit-Limit',config.limit);
+            res.setHeader('X-RateLimit-Remaining',result.remaining);
+            res.setHeader('Retry-After',result.retryAfter);
 
-        const result = await fixedWindow(key,options);
-        console.log(result);
-        res.setHeader('X-RateLimit-Limit',options.limit);
-        res.setHeader('X-RateLimit-Remaining',result.remaining);
-        res.setHeader('Retry-After',result.retryAfter);
 
-        if(!result.allowed){
-            return res.status(429).json({
+            if(!result.allowed){
+                return res.status(429).json({
+                    success : false,
+                    message : "Too many requests."
+                });
+            }
+            next();
+        }catch(error){
+            console.error(error);
+            return res.status(500).json({
                 success : false,
-                message : "Too many requests. Please try again later."
+                message : error.message
             });
         }
-        
-        console.log("Rate Limiter Executed");
-
-        next();
     }
 }
 
-// Right now it's only logging the request. Later, this middleware will call one of our rate-limiting algorithms.

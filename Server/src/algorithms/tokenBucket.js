f9fb0f5a4896cbd68945fc6d805fc2cf36f18afd
Udita Singh <udita1294@gmail.com>
2026-07-11 12:17:27 +0530

    Token Bucket algoritm added

---
+++ Server/src/algorithms/tokenBucket.js
@@ -0,0 +1,52 @@
+import redisClient from '../config/redis.js';
+
+export async function tokenBucket(key,options){
+    const {capacity, refillRate} = options;
+    const redisKey = `tokenBucket:${key}`;
+
+    const now = Date.now();
+
+    let bucket = await redisClient.get(redisKey);
+    if(!bucket){
+        bucket = {
+            tokens: capacity,
+            lastRefill: now
+        };
+    }else{
+        bucket = JSON.parse(bucket);
+    }
+
+    //calculate elapsed time since last refill
+    const elapsed = (now - bucket.lastRefill)/1000;
+
+    //refill tokens
+    const refill = elapsed * refillRate;
+    bucket.tokens = Math.min(capacity , bucket.tokens + refill);
+
+    bucket.lastRefill = now;
+
+    // reject if no tokens
+    if(bucket.tokens < 1){
+        await redisClient.set(redisKey , JSON.stringify(bucket) , {EX : 3600});
+
+        return{
+            allowed: false,
+            count : capacity,
+            remaining: 0,
+            retryAfter : Math.ceil((1 - bucket.tokens)/refillRate)
+        };
+    }
+
+    //consume one token
+    bucket.tokens -= 1;
+
+    await redisClient.set(rediskey,JSON.stringify(bucket),{EX:3600});
+
+    return{
+        allowed: true,
+        count : capacit - Math.floor(bucket.tokens),
+        remaining: Math.floor(bucket.tokens),
+        retryAfter : 0
+    };
+
+}

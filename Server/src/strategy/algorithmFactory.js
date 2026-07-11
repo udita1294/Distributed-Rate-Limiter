1835bf026edd3452674a9bc827b378d1675a8225
Udita Singh <udita1294@gmail.com>
2026-07-11 11:52:43 +0530

    algorithm factory.js modified

--- Server/src/strategy/algorithmFactory.js
+++ Server/src/strategy/algorithmFactory.js
@@ -2,6 +2,7 @@
 // The purpose of this file is:  Given an algorithm name, return the corresponding algorithm function.
 
 import {fixedWindow} from "../algorithms/fixedWindow.js";
+import {slidingWindow} from "../algorithms/slidingWIndow.js";
 
 export function getAlgorithm(algorithmName){
 
@@ -9,6 +10,9 @@ export function getAlgorithm(algorithmName){
         
         case "fixedWindow": 
             return fixedWindow;
+        
+        case "slidingWindow":
+            return slidingWindow;
 
         default:
             throw new Error(`Algorithm ${algorithmName} is not supported`);
